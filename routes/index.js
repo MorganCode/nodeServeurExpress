43ca22f31196939a44ba17c4abd0ce186fccf7cc
MorganCode <morgan.and.code@gmail.com>
2018-12-03 15:54:14 +0100

    changement de mail

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-const nodemailer = require("nodemailer");
-
-var smtpTransport = nodemailer.createTransport("SMTP",{
-  service: "Gmail",
-  auth: {
-      user: "gmail.user@gmail.com",
-      pass: "userpass"
-  }
-});
-
-smtpTransport.sendMail({
-  from: "Deer Wild <deer@wild.com>", // Expediteur
-  to: "contact@wild.com, contact@school.com", // Destinataires
-  subject: "Coucou !", // Sujet
-  text: "Hello world ✔", // plaintext body
-  html: "<b>Hello world ✔</b>" // html body
-}, (error, response) => {
-  if(error){
-      console.log(error);
-  }else{
-      console.log("Message sent: " + response.message);
-  }
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* POST with GET parameters */
+router.get('/yourRecipe', (req, res) => {
 
-router.post("/forms-:formNumbers(\\d+)", function(req, res, next) {
-  res.send(console.log(`Paramètre d'URL: ${req.params.formNumbers} Contenu du GET: ${req.query.level} Contenu du POST: ${req.body.inputContent}`))
-})
+const nodemailer = require('nodemailer');
+
+nodemailer.createTestAccount((err, account) => {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    });
+    let mailOptions = {
+        from: '"mamieCookie" <mamiecookie@yopmail.com>',
+        to: 'supergrandma@yopmail.com',
+        subject: 'Recette cookie',
+        text: 'Tes cookieees !',
+        html: '<b>Tes cookiesss !</b>'
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+});
+
+res.send('ok!');
+});
 
 module.exports = router;
